fbb485f1aa845705aa165c95c5cf89170572c2cc
looocus <13126567829@163.com>
2019-02-25 01:18:45 +0800

    vueproj

---
+++ vueproj/vueproject/src/App.vue
@@ -0,0 +1,74 @@
+<template>
+  <div id="app">
+    <div id="imgdiv">
+      <img src="./assets/logo.png"/>
+    </div>
+    <div id="teach">
+      <p><strong>Submit输出TODOlist</strong><br></p>
+      <p><strong>左键点击条目消除</strong><br></p>
+    </div>
+    <input v-model="msgtext" placeholder="message" />
+    <button @click="handleMessage">Submit</button>
+    <ul>
+      <todoitem
+      v-for="(item, index) of list"
+      :key="index"
+      :content="item"
+      :index="index"
+      @del="DeleteMsg"
+      >
+      </todoitem>
+    </ul>
+  </div>
+</template>
+
+<script>
+import todoitem from './components/todoitem'
+
+export default {
+  data () {
+    return {
+      msgtext: '',
+      list: []
+    }
+  },
+  components: {
+    todoitem
+  },
+  methods: {
+    handleMessage () {
+      this.list.push(this.msgtext)
+      this.msgtext = ''
+      document.getElementsByTagName('input')[0].focus()
+    },
+    DeleteMsg (index) {
+      this.list.splice(index, 1)
+    }
+  }
+}
+</script>
+
+<style scoped>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  text-align: center;
+  color: #2c3e50;
+  height: 100%;
+}
+#imgdiv {
+  width: 100%;
+}
+#teach {
+  top: 60px;
+  right: 60px;
+  position: absolute;
+  text-align: left;
+}
+ul {
+  top: 1%;
+  width: 50%;
+  margin: 0 auto;
+  position: relative;
+}
+</style>
---
+++ vueproj/vueproject/src/components/todoitem.vue
@@ -0,0 +1,47 @@
+
+<template>
+  <div>
+    <div id="bgstyle" @click="handelDelete"></div>
+    <div id="txstyle">
+      <li><strong>{{ content }}</strong></li>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      msg: 'Done'
+    }
+  },
+  props: ['content', 'index'],
+  methods: {
+    handelDelete () {
+      this.$emit('del', this.index)
+      // alert(this.index)
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+#bgstyle {
+  background-color: white;
+  filter: blur(15px);
+  filter: opacity(0.2);
+  position: absolute;
+  width: 50%;
+  height: 20px;
+  right: 25%;
+}
+#txstyle {
+  font-size: 20px;
+  width: 50%;
+  margin: 0 auto;
+  border-radius: 3px;
+  display: block;
+  text-align: left;
+}
+</style>
